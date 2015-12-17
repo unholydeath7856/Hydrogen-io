ea0216f11fd8d66f187ae5697457925aad191f3b
unholydeath785 <evan@coulson-orellana.com>
2015-12-17 14:24:19 -0800

    Uploaded all files to git

---
+++ Hydrogen.io/Assets/Scripts/JS/functions.js
@@ -0,0 +1,17 @@
+function smoothScool (duration) {
+    $('a[href^="#"]').on('click', function(event) {
+
+        var target = $( $(this).attr('href') );
+
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, duration);
+        }
+    });
+}
+
+$(document).ready(function () {
+  smoothScool(1000);
+});
